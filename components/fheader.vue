6064455fe1addd370491ed1bd8ddc4a663dbc252
Fernando Meseguer <fernandomfdz@gmail.com>
2023-04-30 10:28:41 +0200

    Init

---
+++ components/fheader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out"
+  >
+    <div class="max-w-6xl mx-auto px-5 sm:px-6">
+      <div class="flex items-center justify-between h-16 md:h-20">
+        <div class="shrink-0 mr-4">
+          <a class="block" aria-label="Cruip" href="/"
+            ><svg
+              class="w-8 h-8"
+              viewBox="0 0 32 32"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <radialGradient
+                  cx="21.152%"
+                  cy="86.063%"
+                  fx="21.152%"
+                  fy="86.063%"
+                  r="79.941%"
+                  id="footer-logo"
+                >
+                  <stop stop-color="#4FD1C5" offset="0%"></stop>
+                  <stop stop-color="#81E6D9" offset="25.871%"></stop>
+                  <stop stop-color="#338CF5" offset="100%"></stop>
+                </radialGradient>
+              </defs>
+              <rect
+                width="32"
+                height="32"
+                rx="16"
+                fill="url(#footer-logo)"
+                fill-rule="nonzero"
+              ></rect></svg
+          ></a>
+        </div>
+      </div>
+    </div>
+</div>
+</template>
---
+++ components/hero.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="max-w-6xl mx-auto px-4 sm:px-6">
+    <div class="pt-6 pb-12 md:pt-40 md:pb-20">
+      <div class="text-center pb-12 md:pb-16">
+        <h1 class="z-20 text-6xl md:text-8xl font-extrabold leading-tighter tracking-tighter mb-4 aos-init aos-animate text-slate-100">
+          Haz una paella
+            <div class="mt-4 flex flex-wrap gap-0 md:gap-4 items-center justify-center">
+                <span :class="{'opacity-20' : index !== 0}" class="transition-all ease-in-out delay-200 bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-400">Buena.</span>
+                <span :class="{'opacity-20' : index !== 1}" class="transition-all ease-in-out delay-200 bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Fácil.</span>
+                <span :class="{'opacity-20' : index !== 2}" class="transition-all ease-in-out delay-200 bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-yellow-400">Exacta.</span>
+            </div>
+        </h1>
+        <div class="z-20 max-w-3xl mx-auto">
+          <p class="text-xl mb-8 aos-init aos-animate">
+            No pierdas ingredientes. No pierdas tiempo, introduce el número de comensales y deja de calcular a ojo
+          </p>
+          <div class="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center aos-init aos-animate">
+            <div>
+              <a
+                class="btn"
+                href="#0"
+                >Comenzar</a
+              >
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import {ref} from 'vue';
+export default {
+    setup() {
+        const index = ref(0);
+
+        setInterval(() => {
+            if(index.value + 1 > 2) {
+                index.value = 0;
+            } else {
+                index.value++;
+            }
+        }, 3000);
+        return {index};
+    },
+}
+</script>
