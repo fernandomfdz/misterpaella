a6e67ac224ffc08c3a66c9d6b4dbc53fd5f27304
Fernando Meseguer <fernandomfdz@gmail.com>
2025-08-27 19:09:51 +0200

    Seo

--- pages/index.vue
+++ pages/index.vue
@@ -10,8 +10,22 @@
   useHead({
     title: 'Mister Paella',
     meta: [
-      { name: 'description', content: 'Haz una paella. No pierdas ingredientes. No pierdas tiempo, introduce el número de comensales y deja de calcular a ojo' }
+      { name: 'description', content: 'Haz una paella. No pierdas ingredientes. No pierdas tiempo, introduce el número de comensales y deja de calcular a ojo' },
+      { hid: 'og-description', property: 'og:description', content: 'Haz una paella. No pierdas ingredientes. No pierdas tiempo, introduce el número de comensales y deja de calcular a ojo'},
+      { hid: 'twitter-card', property: 'twitter:card', content: '/opengraph-image.png' },
+      { hid: 'og-title', property: 'og:title', content: 'Mister Paella'},
+      { hid: 'og-type', property: 'og:type', content: 'website' },
+      { hid: 'og-image', property: 'og:image', content: '/opengraph-image.png'},
     ]
   });
+
+  useSeoMeta({
+    title: 'Mister Paella',
+    ogTitle: 'Mister Paella',
+    description: 'Haz una paella. No pierdas ingredientes. No pierdas tiempo, introduce el número de comensales y deja de calcular a ojo',
+    ogDescription: 'Haz una paella. No pierdas ingredientes. No pierdas tiempo, introduce el número de comensales y deja de calcular a ojo',
+    ogImage: '/opengraph-image.png',
+    twitterCard: '/opengraph-image.png',
+  })
 
 </script>
